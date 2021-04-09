91c813bbc8871aa4387e2a28e79cd346982428ed
Akram Izzeldin <akram.izz.kh@gmail.com>
2021-04-09 23:18:48 +0200

    fix encoder and decoder and add dpcm step

--- coder.py
+++ coder.py
@@ -9,13 +9,15 @@ def encode(img_file: str) -> np.ndarray:
   blocks = blocks_split(yuv)
   coeffs = dct2d_n(blocks)
   q = quantize(coeffs, precomputed_quantization_table)
-  zz = zigzag(q)
-  return q
+  dc_encoded = dpcm(q)
+  zz = zigzag(dc_encoded)
+  return zz
 
-def decode(inp: np.ndarray) -> np.ndarray:
-  rq = unzigzag(inp)
+def decode(inp: np.ndarray, width: int, height: int) -> np.ndarray:
+  rdc_encoded = unzigzag(inp)
+  q = un_dpcm(rdc_encoded)
   rcoeffs = dequantize(q, precomputed_quantization_table)
   rblocks = dct2d_n(rcoeffs, inverse=True)
-  ryuv = de_blocks(rblocks)
+  ryuv = de_blocks(rblocks, width, height)
   rimg = yCbCr_to_rgb(ryuv)
   return rimg
