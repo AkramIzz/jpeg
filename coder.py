dfd74ceea6b3bfb06c610be8a36d7e297b580bc1
Akram Izzeldin <akram.izz.kh@gmail.com>
2021-04-09 21:15:25 +0200

    partial implementation of the encoder and decoder

---
+++ coder.py
@@ -0,0 +1,19 @@
+#%%
+from utils import *
+import numpy as np
+
+def encode(img_file: str) -> np.ndarray:
+  img = image_to_numpy(img_file)
+  yuv = rgb_to_yCbCr(img)
+  yuv = chroma_subsampling(yuv)
+  blocks = blocks_split(yuv)
+  coeffs = dct2d_n(blocks)
+  q = quantize(coeffs, precomputed_quantization_table)
+  return q
+
+def decode(q: np.ndarray) -> np.ndarray:
+  rcoeffs = dequantize(q, precomputed_quantization_table)
+  rblocks = dct2d_n(rcoeffs, inverse=True)
+  ryuv = de_blocks(rblocks)
+  rimg = yCbCr_to_rgb(ryuv)
+  return rimg
